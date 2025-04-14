86026f327364787bec679a7f737d863bdc41ecbf
Fanil Borkhanov <Fanil.Borkhanov@vmedia.ca>
2025-04-14 12:18:42 +0300

    Start test project

---
+++ src/components/ui/BaseCard.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+    <v-card title="Card title" text="..." variant="tonal"></v-card>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles/main.css";
+import "./assets/styles/main.scss";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+import App from "./App.vue";
+// Создаем Vuetify экземпляр
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+const app = createApp(App);
+app.use(vuetify);
+app.use(createPinia());
+app.mount("#app");
